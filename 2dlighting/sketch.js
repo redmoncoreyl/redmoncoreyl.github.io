48499628bf6d9ddd7587197e95f7db0b5b3d9970
Corey Redmon <redmoncoreyl@gmail.com>
2018-11-26 10:27:53 -0500

    fix boundaries

--- 2dlighting/sketch.js
+++ 2dlighting/sketch.js
@@ -1,20 +1,3 @@
-// shape locations
-// 204 325
-// 363 216
-// 452 408
-//
-// 294 721
-// 509 688
-// 531 571
-// 638 587
-// 649 774
-// 445 786
-//
-// 671 105
-// 681 262
-// 795 346
-// 782 39
-
 let segments = [];
 
 function setup() {
@@ -49,10 +32,6 @@ function setup() {
   segments.push(new Segment(550, 50, 600, 50));
 }
 
-function mousePressed() {
-  console.log(mouseX, mouseY);
-}
-
 function ccw(p1, p2, p3) {
   return (p2.x - p1.x)*(p3.y - p1.y) - (p2.y - p1.y)*(p3.x - p1.x);
 }
@@ -62,6 +41,9 @@ function cross2D(r1, r2) {
 }
 
 function calculateLight(segs, mx, my) {
+  if (mx < 0 || mx > width || my < 0 || my > height) {
+    return [];
+  }
   let rays = [];
   let m = createVector(mx, my);
   for (let i = 0; i < segs.length; i++) {
@@ -101,19 +83,20 @@ function calculateLight(segs, mx, my) {
 }
 
 function draw() {
-  // put drawing code here
   background(0);
   for (s of segments) {
     s.show();
   }
   let lightPoints = calculateLight(segments, mouseX, mouseY);
-  push();
-  noStroke();
-  fill(255);
-  beginShape();
-  for (let i = 0; i < lightPoints.length; i++) {
-    vertex(lightPoints[i].x, lightPoints[i].y);
+  if (lightPoints.length != 0) {
+    push();
+    noStroke();
+    fill(255);
+    beginShape();
+    for (let i = 0; i < lightPoints.length; i++) {
+      vertex(lightPoints[i].x, lightPoints[i].y);
+    }
+    endShape(CLOSE);
+    pop();
   }
-  endShape(CLOSE);
-  pop();
 }
