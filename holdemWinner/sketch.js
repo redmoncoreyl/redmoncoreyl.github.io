96014c49548524ba7b605b4470e94929d9e80b2c
Corey Redmon <redmoncoreyl@gmail.com>
2025-02-21 07:31:08 -0500

    removes drawing circle tests

--- holdemWinner/sketch.js
+++ holdemWinner/sketch.js
@@ -6,9 +6,6 @@ document.addEventListener('contextmenu', event => event.preventDefault());
 
 let CARD_FONT;
 let game;
-let drawRedRect = false;
-let circleX = 0;
-let circleY = 0;
 let ignoreMouseDown = false;
 
 function preload() {
@@ -24,10 +21,6 @@ function preload() {
 function mousePressed(event) {
 	if (event.type === 'touchstart') ignoreMouseDown = true;
 	if (ignoreMouseDown && event.type === 'mousedown') return;
-
-	drawRedRect = !drawRedRect;
-	circleX = mouseX;
-	circleY = mouseY;
 }
 
 function setup() {
@@ -38,10 +31,4 @@ function setup() {
 function draw() {
 	background(3, 115, 55);
 	game.draw();
-	if (drawRedRect) {
-		noStroke();
-		fill(255, 0, 0, 120);
-		rect(0, 0, width, height);
-		ellipse(circleX, circleY, width/8);
-	}
 }
