7dd438886ca9647e5d4147bb8e9b1594f8b0672a
Corey Redmon <redmoncoreyl@gmail.com>
2025-03-03 18:27:12 -0500

    draw total time buttons

--- holdemWinner/TimeTrialGame.js
+++ holdemWinner/TimeTrialGame.js
@@ -18,6 +18,10 @@ class TimeTrialGame {
 		this.buttonHoverColor = (new p5(() => {})).color(2, 20, 4);
 		this.buttonTextColor = (new p5(() => {})).color(235);
 		this.settingsStartButton = new Button('Start', 0, 0, 0, 0,  0, 0, this.buttonColor, this.buttonHoverColor, this.buttonTextColor, null);
+		this.totalTimeDecButton = new Button('-', 0, 0, 0, 0, 0, 0, this.buttonColor, this.buttonHoverColor, this.buttonTextColor, null);
+		this.totalTimeIncButton = new Button('+', 0, 0, 0, 0, 0, 0, this.buttonColor, this.buttonHoverColor, this.buttonTextColor, null);
+
+		this.totalTime = 120;
 		
 		this.resize(screenWidth, screenHeight);
 	}
@@ -33,15 +37,15 @@ class TimeTrialGame {
 
 		this.titleSize = this.isWideScreen ? 0.055 * this.menuHeight : 0.061 * this.menuWidth;
 		this.titleCenterX = (this.menuLeft + this.menuRight)/2;
-		this.titleTopY = 0.5*this.titleSize;
+		this.verticalPadding = 0.5*this.titleSize;
+		this.titleTopY = this.verticalPadding;
 
-		let buttonHeight = 0.06 * this.menuHeight;
+		this.buttonHeight = 0.06 * this.menuHeight;
 		let buttonLeftX = 0.2 * this.menuWidth + this.menuLeft;
-		let buttonTopY = this.menuHeight - buttonHeight - 0.5*this.titleSize;
+		let buttonTopY = this.menuHeight - this.buttonHeight - this.verticalPadding;
 		let buttonWidth = 0.6 * this.menuWidth;
-		let cornerRadius = 0.2*buttonHeight;
-		let buttonStrokeWeight = 0.03 * buttonHeight;
-		this.settingsStartButton.resize(buttonLeftX, buttonTopY, buttonWidth, buttonHeight, cornerRadius, buttonStrokeWeight);
+		this.cornerRadius = 0.2*this.buttonHeight;
+		this.settingsStartButton.resize(buttonLeftX, buttonTopY, buttonWidth, this.buttonHeight, this.cornerRadius, 0);
 
 		this.helpTextLeftX = this.menuLeft + 0.07*this.menuWidth;
 		this.helpTextTopY = this.menuTop + this.titleTopY + this.titleSize*1.9;
@@ -50,8 +54,22 @@ class TimeTrialGame {
 		this.helpTextRectPadding = this.menuWidth*.015;
 		this.helpTextRectX = this.helpTextLeftX - this.helpTextRectPadding;
 		this.helpTextRectY = this.helpTextTopY - this.helpTextRectPadding;
-		this.helpTextRectHeight = buttonTopY - this.helpTextRectY - 0.5*this.titleSize;
+		this.helpTextRectHeight = buttonTopY - this.helpTextRectY - this.verticalPadding;
 		this.helpTextRectWidth = this.helpTextWidth + 2*this.helpTextRectPadding;
+
+		// total time
+		let settingsTextHeight = 3*this.helpTextSize;
+		let totalTimeDecButtonX = this.helpTextLeftX;
+		let totalTimeDecButtonY = this.helpTextTopY + settingsTextHeight + this.verticalPadding;
+		let totalTimeDecButtonWidth = this.buttonHeight;
+		let totalTimeIncButtonX = this.helpTextLeftX + this.helpTextWidth - totalTimeDecButtonWidth;
+		this.totalTimeDecButton.resize(totalTimeDecButtonX, totalTimeDecButtonY, totalTimeDecButtonWidth, this.buttonHeight, this.cornerRadius, 0);
+		this.totalTimeIncButton.resize(totalTimeIncButtonX, totalTimeDecButtonY, totalTimeDecButtonWidth, this.buttonHeight, this.cornerRadius, 0);
+		this.totalTimeRectWidth = this.helpTextWidth - 2*this.helpTextRectPadding - 2*totalTimeDecButtonWidth;
+		this.totalTimeRectX = screenWidth/2 - this.totalTimeRectWidth/2;
+		this.totalTimeRectY = totalTimeDecButtonY;
+		this.totalTimeRectHeight = this.buttonHeight;
+
 	}
 
 	draw(p5Instance) {
@@ -75,14 +93,27 @@ class TimeTrialGame {
 		p5Instance.push();
 		p5Instance.fill(193, 225, 195);
 		p5Instance.noStroke();
-		p5Instance.rect(this.helpTextRectX, this.helpTextRectY, this.helpTextRectWidth, this.helpTextRectHeight, this.helpTextRectPadding);
+		p5Instance.rect(this.helpTextRectX, this.helpTextRectY, this.helpTextRectWidth, this.helpTextRectHeight, this.cornerRadius);
 		p5Instance.fill(0);
 		p5Instance.textAlign(p5Instance.LEFT, p5Instance.TOP);
 		p5Instance.textSize(this.helpTextSize);
 		p5Instance.textLeading(this.helpTextSize);
-		p5Instance.text('Settings', this.helpTextLeftX, this.helpTextTopY, this.helpTextWidth);
+		p5Instance.text('Settings\n\nTotal time:', this.helpTextLeftX, this.helpTextTopY, this.helpTextWidth);
+		p5Instance.pop();
+
+		// draw total time buttons
+		p5Instance.push();
+		p5Instance.noStroke();
+		p5Instance.fill(15, 80, 30);
+		p5Instance.rect(this.totalTimeRectX, this.totalTimeRectY, this.totalTimeRectWidth, this.totalTimeRectHeight, this.cornerRadius);
+		p5Instance.fill(this.buttonTextColor);
+		p5Instance.textAlign(p5Instance.CENTER, p5Instance.CENTER);
+		p5Instance.textSize(this.helpTextSize);
+		p5Instance.text(this.totalTime + ' s', this.totalTimeRectX, this.totalTimeRectY, this.totalTimeRectWidth, this.totalTimeRectHeight)
 		p5Instance.pop();
 
 		this.settingsStartButton.draw(p5Instance);
+		this.totalTimeDecButton.draw(p5Instance);
+		this.totalTimeIncButton.draw(p5Instance);
 	}
 }
